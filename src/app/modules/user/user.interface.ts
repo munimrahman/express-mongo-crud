8f68db2a6488312bf744f6a6c3b85039a3270f3a
munimrahman <munimrh@gmail.com>
2023-11-25 17:00:14 +0600

    user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,29 @@
+interface IAddress {
+  street: string;
+  city: string;
+  country: string;
+}
+
+interface IOrder {
+  productName: string;
+  price: number;
+  quantity: number;
+}
+
+interface IUser {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: IAddress;
+  orders: IOrder[];
+}
+
+export { IAddress, IOrder, IUser };
